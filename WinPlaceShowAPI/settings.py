4d635499e39d09cb87714e3dd527eb4bbba1e457
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-04-19 01:10:30 -0700

    staticfiles

--- WinPlaceShowAPI/settings.py
+++ WinPlaceShowAPI/settings.py
@@ -209,11 +209,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 django_heroku.settings(locals())
