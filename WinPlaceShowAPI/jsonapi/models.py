d0cca10e079d0c752916c3b93fc1259361151ba9
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-04-18 21:33:09 -0700

    typo

--- WinPlaceShowAPI/jsonapi/models.py
+++ WinPlaceShowAPI/jsonapi/models.py
@@ -8,20 +8,25 @@ Races and Horses
 """
 # Create your models here.
 
-class Races(models.Model):
-    date = models.DateField()
+
+class Race(models.Model):
+    date = models.DateField(blank=True)
+    year = models.PositiveSmallIntegerField()
     race_name = models.CharField(max_length=200)
-    horses = ArrayField(models.CharField(max_length=200))
+    city = models.CharField(max_length=200)
+    horses = ArrayField(models.CharField(max_length=200), blank=True)
     TC_PRERACES = 'Pre'
     UPCOMING_TC_RACES = 'TCR'
-    PREV_TC = 'Past'
+    PREV_TC = 'PastTCR'
+    PAST = 'PastPre'
     CATEGORY_CHOICES = (
+        (PAST, "Past Years' Triple Crown Prep Races")
         (PREV_TC, "Past Years' Triple Crown Races"),
         (UPCOMING_TC_RACES, "This Year's Triple Crown Races"),
         (TC_PRERACES, "Races Leading Up To This Year's Triple Crown")
     )
-    category = models.CharField(max_length=4, choices=CATEGORY_CHOICES)
-    results = JSONField()
+    category = models.CharField(max_length=4, choices=CATEGORY_CHOICES, blank=True)
+    results = JSONField(blank=True)
 
     class JSONAPIMeta:
         resource_name = 'races'
@@ -31,8 +36,8 @@ class Races(models.Model):
         return DjangoJSONEncoder().encode(self)
 
 
-class Horses(models.Model):
-    name = models.CharField(max_length=200)
+class Horse(models.Model):
+    name = models.CharField(max_length=200, unique=True)
     age = models.PositiveSmallIntegerField()
     owner = models.CharField(max_length=200)
     sire = models.CharField(max_length=200)
@@ -42,6 +47,7 @@ class Horses(models.Model):
     career_places = models.PositiveSmallIntegerField()
     career_shows = models.PositiveSmallIntegerField()
     breed = models.CharField(max_length=50)
+    races = models.ManyToManyField(Race)
 
     class JSONAPIMeta:
         resource_name = 'races'
